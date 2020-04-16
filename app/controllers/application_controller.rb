cbd5745f585aa3dd4c929761832a1bc6de9a9cf7
Tatyana Scotce <tscotce@gmail.com>
2020-04-16 00:12:44 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
-  configure do
-    set :views, Proc.new { File.join(root, "../views/") }
-    enable :sessions unless test?
+  set :views, Proc.new { File.join(root, "../views/") }
+    
+  configure do  
+    enable :sessions 
     set :session_secret, "secret"
   end
 
@@ -11,15 +12,25 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    # binding.pry
+    user = User.find_by(username: params[:username])
+    if user 
+    session[:user_id] = user.id
+    redirect to :'/account'
+    elsif redirect to :'/'
+    end
   end
 
   get '/account' do
-
+    if User.is_logged_in?
+      erb :account
+    elsif redirect to :'/login'
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to :'/'
   end
 
 
